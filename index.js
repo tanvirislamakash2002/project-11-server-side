091cffc4035d7d6a000ffc416e6cca441eb3ad69
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-06-08 22:14:30 +0600

    article post method added successfully

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+require('dotenv').config()
+const cors= require('cors')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 3000
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const database = client.db('ph-a11-article')
+    const articlesCollection = database.collection('articles-collection')
+    const commentsCollection = database.collection('comments-collection')
+
+    app.post('/post-article',async(req, res)=>{
+        const articleData = req.body;
+        const result =await articlesCollection.insertOne(articleData)
+        res.send(result)
+    })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send('a new era of knowledge begun')
+})
+
+app.listen(port,()=>{
+    console.log('server in on the go')
+})
